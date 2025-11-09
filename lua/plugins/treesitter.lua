a9388c433ac4d9564b3b0c263d034aa93410627f
Ahmad Hasanudin <hasan13.blabla@gmail.com>
2025-11-10 00:17:23 +0700

    fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,54 +4,30 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
+        "go",
+        "html",
+        "php",
         "bash",
-        "c",
-        "cpp",
-        "css",
-        "diff",
         "dockerfile",
+        "diff",
         "git_config",
         "git_rebase",
         "gitattributes",
         "gitcommit",
         "gitignore",
-        "go",
-        "html",
-        "http",
-        "java",
-        "javascript",
-        "jsdoc",
         "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "php",
-        "phpdoc",
-        "python",
-        "query",
-        "regex",
-        "ruby",
-        "rust",
+        "javascript",
+        "css",
         "sql",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
       })
-      
+
       opts.highlight = opts.highlight or {}
       opts.highlight.enable = true
       opts.highlight.additional_vim_regex_highlighting = false
-      
+
       opts.indent = opts.indent or {}
       opts.indent.enable = true
-      
+
       return opts
     end,
   },
